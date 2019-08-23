4a40ce7585eb9858253a6a896d214f07b2d2ceac
Priscilla Rodrigues Martins <angeleyes.ffx@hotmail.com>
2019-08-23 12:45:29 -0300

    Update homepage_steps.py

--- features/steps/homepage_steps.py
+++ features/steps/homepage_steps.py
@@ -1,5 +1,7 @@
 from behave import *
 from nose.tools import assert_equals
+from selenium.webdriver.common.by import By
+from features.datapool import DATA_ACCESS
 
 from features.pages.basepage import BasePage
 from features.pages.homepage import HomePage
@@ -7,12 +9,11 @@ from features.pages.homepage import HomePage
 
 @given(u'I navigate to the Google Home page')
 def navigate_to_home_page(context):
-    # context.browser.get("http://google.com")
+    context.home_page = BasePage.instance_page(HomePage,context)
     assert_equals(context.browser.current_url, "{}".format(context.home_page.project_url))
-    raise NotImplementedError(u'STEP: Given I navigate to the Google Home page')
 
 
-@when(u'I search for "blabla"')
+@when(u'I search for {search_data}')
 def step_impl(context):
     raise NotImplementedError(u'STEP: When I search for "blabla"')
 
